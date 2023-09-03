6bf31aefe85433218a4cb91f66b0868514a66bd1
Alex Nesterov <podixqr@gmail.com>
2023-09-03 21:36:15 +0200

    feat: add BE

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Paper from "@mui/material/Paper";
 import { Layout } from "./components/Layout";
 import { Container } from "@mui/material";
 import { useScrollUp } from "./hooks/useScrollUp";
-import { putData } from "./api/api";
 
 export const App = () => {
   useScrollUp();
-
-  useEffect(() => {
-    (async () => {
-      const statisticLC = localStorage.getItem("statistic");
-      const lastOpenQuestionLC = localStorage.getItem("last-open-question");
-      const wrongAnswersLC = localStorage.getItem("wrong-answers");
-
-      if (statisticLC && lastOpenQuestionLC && wrongAnswersLC) {
-        const statistic = JSON.parse(statisticLC);
-        const lastOpenQuestion = JSON.parse(lastOpenQuestionLC);
-        const wrongAnswers = JSON.parse(wrongAnswersLC);
-
-        await putData({ lastOpenQuestion, statistic, wrongAnswers });
-      }
-    })();
-  }, []);
 
   return (
     <Paper
@@ -30,7 +13,9 @@ export const App = () => {
         pt: 3,
       }}
     >
-      <Container>{/* <Layout /> */}</Container>
+      <Container>
+        <Layout />
+      </Container>
     </Paper>
   );
 };
